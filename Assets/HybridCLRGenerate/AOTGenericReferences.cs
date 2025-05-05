e05ca0e0eaf0c9ec9d509dd132cc3641132dca0f
GTuanTuan <1747715394@qq.com>
2025-05-05 16:20:00 +0800

    ReadMe

--- Assets/HybridCLRGenerate/AOTGenericReferences.cs
+++ Assets/HybridCLRGenerate/AOTGenericReferences.cs
@@ -6,7 +6,10 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
 	{
 		"Cinemachine.dll",
+		"System.Core.dll",
+		"System.dll",
 		"Unity.InputSystem.dll",
+		"Unity.ResourceManager.dll",
 		"UnityEngine.CoreModule.dll",
 		"mscorlib.dll",
 	};
@@ -16,23 +19,40 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// }} 
 
 	// {{ AOT generic types
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>>
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// DelegateList<float>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,object>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Action<float>
 	// System.Action<object>
-	// System.Collections.Generic.ArraySortHelper<object>
-	// System.Collections.Generic.Comparer<object>
 	// System.Collections.Generic.EqualityComparer<object>
+	// System.Collections.Generic.HashSet.Enumerator<object>
+	// System.Collections.Generic.HashSet<object>
 	// System.Collections.Generic.ICollection<object>
-	// System.Collections.Generic.IComparer<object>
 	// System.Collections.Generic.IEnumerable<object>
 	// System.Collections.Generic.IEnumerator<object>
 	// System.Collections.Generic.IEqualityComparer<object>
-	// System.Collections.Generic.IList<object>
-	// System.Collections.Generic.List.Enumerator<object>
-	// System.Collections.Generic.List<object>
-	// System.Collections.Generic.ObjectComparer<object>
+	// System.Collections.Generic.LinkedList.Enumerator<object>
+	// System.Collections.Generic.LinkedList<object>
+	// System.Collections.Generic.LinkedListNode<object>
 	// System.Collections.Generic.ObjectEqualityComparer<object>
-	// System.Collections.ObjectModel.ReadOnlyCollection<object>
-	// System.Comparison<object>
+	// System.Func<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Func<object,UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Func<object,object>
+	// System.Func<object>
 	// System.Predicate<object>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<object>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<object>
+	// System.Runtime.CompilerServices.TaskAwaiter<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Runtime.CompilerServices.TaskAwaiter<object>
+	// System.Threading.Tasks.Task<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Threading.Tasks.Task<object>
+	// System.Threading.Tasks.TaskCompletionSource<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// System.Threading.Tasks.TaskCompletionSource<object>
 	// UnityEngine.InputSystem.InputBindingComposite<UnityEngine.Vector2>
 	// UnityEngine.InputSystem.InputBindingComposite<float>
 	// UnityEngine.InputSystem.InputControl<UnityEngine.Vector2>
@@ -40,6 +60,13 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// UnityEngine.InputSystem.InputProcessor<UnityEngine.Vector2>
 	// UnityEngine.InputSystem.InputProcessor<float>
 	// UnityEngine.InputSystem.Utilities.InlinedArray<object>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass60_0<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass61_0<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle.<>c<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
+	// UnityEngine.ResourceManagement.Util.GlobalLinkedListNodeCache<object>
+	// UnityEngine.ResourceManagement.Util.LinkedListNodeCache<object>
 	// }}
 
 	public void RefMethods()
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# HybridCLR_Addressable
+# Boot为启动场景
+![alt text](Img/image-5.png)![alt text](Img/image-6.png)
+# HybridCLR
+## 1.顶部菜单栏HybridCLR/Installer
+![alt text](Img/image.png)
+## 2.点击Install下载，等待结束
+![alt text](Img/image-1.png)
+## 3.执行一次Generate/All
+![alt text](Img/image-2.png)
+# nginx
+## 1.启动nginx.exe
+![alt text](Img/image-7.png)![alt text](Img/image-8.png)
+## 2.打开浏览器访问localhost:7777
+![alt text](Img/image-9.png)
+# Addressable
+## 1.打包出来效果(Build/Exe/HybridCLR_Addressable.exe)
+![alt text](Img/image-10.png)![alt text](Img/image-11.png)![alt text](Img/image-12.png)
+## 2.构建更新
+### 设置为“preload”标签的资源再进游戏前更新，其他为按需加载
+![alt text](Img/image-13.png)
+### 打包点击顶部菜单栏Tools/增量构建 或 全部重新打包
+![alt text](Img/image-14.png)
